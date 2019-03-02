5bf49b508bb4d21e9686e9f0985c115a35805b38
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2019-03-02 23:13:25 +0900

    look at duct.module.web/api in config.edn

---
+++ foobar+api/src/foobar+api/main.clj
@@ -0,0 +1,12 @@
+(ns foobar+api.main
+  (:gen-class)
+  (:require [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "foobar+api/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))))
